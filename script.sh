828ac4996d68c9b2bd717f1516d87fa5f474fd30
onkjm <onkjm.dev@gmail.com>
2023-05-15 07:02:57 +0900

    mod

--- script.sh
+++ script.sh
@@ -1,39 +1,27 @@
-export PATH="/opt/homebrew/bin/pandoc:$PATH"
-which pandoc
-echo =========================
-echo set path to pandoc
-echo =========================
-
-make html
 echo =========================
 echo make html
 echo =========================
+make html
 
-cp -R $1/s/build/html/* /usr/local/var/www/myapi/
 echo =========================
 echo deploy html
 echo =========================
+cp -R $1/s/build/html/* /usr/local/var/www/myapi/
 
-/opt/homebrew/bin/nginx -s reload
 echo =========================
 echo restart nginx
 echo =========================
+/opt/homebrew/bin/nginx -s reload
 
-export MEILISEARCH_HOST_URL=$2
-export MEILISEARCH_API_KEY=$3
-set
 echo =========================
 echo set env vars
 echo =========================
- 
-cd /Users/on/docs-scraper
-source .venv/bin/activate
-#git clone https://github.com/meilisearch/docs-scraper
-#cd docs-scraper
-#pip install pipenv
-#pipenv install
-#ls
-pipenv run ./docs_scraper $1/s/docs-scraper/myapi.json
+export MEILISEARCH_HOST_URL=$2
+export MEILISEARCH_API_KEY=$3
+
 echo =========================
 echo docs scraping
 echo =========================
+cd /Users/on/docs-scraper
+source .venv/bin/activate
+pipenv run ./docs_scraper $1/s/docs-scraper/myapi.json

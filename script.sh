fae39bf0c4ae69e026cab134bd5f931504235ea2
onkjm <onkjm.dev@gmail.com>
2023-05-15 14:37:29 +0900

    mod

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ echo install dependencies
 echo =========================
 python3 -m venv .venv
 source .venv/bin/activate
-pip install requirements.txt
+pip install -r requirements.txt
 
 echo =========================
 echo make html
